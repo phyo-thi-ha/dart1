c23b7d9999389034dc174d45e8cad9a2decd9499
phyo-thi-ha <phyothiha.900@gmail.com>
2025-05-21 11:33:57 +0630

    async

--- test.dart
+++ test.dart
@@ -1,21 +1,14 @@
-mixin canFly {
-  void fly() {
-    print("I can fly");
-  }
-}
-
-mixin canWalk {
-  void walk() {
-    print("I can walk");
-  }
-}
-
-class Bird with canFly, canWalk {
-  // Bird class can use both canFly and canWalk mixins
-}
-
 void main() {
-  var bird = Bird();
-  bird.fly();
-  bird.walk();
+  print("Start");
+  getData();
+  print("End");
+}
+
+void getData() async {
+  String data = await middleFunction();
+  print(data);
+}
+
+Future<String> middleFunction() {
+  return Future.delayed(Duration(seconds: 5), () => "Hello");
 }
