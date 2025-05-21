8b7eefb4c1632f46f1ebaeb10f69045b490662de
phyo-thi-ha <phyothiha.900@gmail.com>
2025-05-21 13:30:53 +0630

    shalf

--- test.dart
+++ test.dart
@@ -1,14 +1,18 @@
-void main() {
-  print("Start");
-  getData();
-  print("End");
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+void main() async {
+  var handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(_echoRequest);
+
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
 
-void getData() async {
-  String data = await middleFunction();
-  print(data);
-}
-
-Future<String> middleFunction() {
-  return Future.delayed(Duration(seconds: 5), () => "Hello");
-}
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
